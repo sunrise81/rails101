3f8f1c139c0994c5dec8a55bb246a486e3ca64aa
sunrise <luoruijunblue@hotmail.com>
2016-12-18 21:56:47 +0800

    implement post create function

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -11,6 +11,7 @@ class GroupsController < ApplicationController
 
   def show
     @group = Group.find(params[:id])
+    @posts = @group.posts
   end
 
   def edit
@@ -52,7 +53,7 @@ class GroupsController < ApplicationController
     if current_user != @group.user
       redirect_to root_path, alert: "You have no permission."
     end
-    
+
     @group.destroy
     flash[:alert] = "Group deleted"
     redirect_to groups_path
